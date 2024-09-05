b11f16222b1609e3da554ca92ce739b2f1a5f9f7
Onur Gedik <gazi.onur98@gmail.com>
2024-09-05 14:02:30 +0300

    Initial commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_TaskMasterPro_FILE C:/Users/pc/source/QtProjects/TaskMasterPro/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/TaskMasterPro.exe)
+set(__QT_DEPLOY_TARGET_TaskMasterPro_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_TaskMasterPro_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
